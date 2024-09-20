997936927161088c10cea4dc84dc3d22762dc63a
I hate iu <tranhung10122003@gmail.com>
2024-09-20 18:31:23 +0700

    feat: add some functions

---
+++ Assets/Scripts/MainCharacter/BackgroundMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace MainCharacter
+{
+    public class BackgroundMovement:MonoBehaviour
+    {
+        private Material mat;
+        private float distance;
+        [Range(0f,100f)]
+        public float speed;
+
+        private void Start()
+        {
+            mat = GetComponent<Renderer>().material;
+        }
+        
+        void Update()
+        {
+            distance = Time.deltaTime * speed;
+            mat.SetTextureOffset("_MainTex", Vector2.right * distance); 
+        }
+    }
+}
---
+++ Assets/Tile/64x64 tileset/64x64 tileset/readme.txt
@@ -0,0 +1,5 @@
+Hello, this is my "ULLTRA HD 4K SUPER REALISTIC" tileset. 
+Please subscribe me on itch.io, also you can donate some DoLaRz if you want.
+If you have questions, my Email    AxoLotie28@gmail.com
+Enjoy
+             AxoLotie ^^
