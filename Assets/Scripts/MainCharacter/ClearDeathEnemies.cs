c74459cd961e6324e2a3cf16970f14434c77cf81
OLogN <vinhhiepbn2003.work@gmail.com>
2024-10-04 21:45:56 +0700

    feat: add new Tag, new script files

---
+++ Assets/Scripts/MainCharacter/ClearDeathEnemies.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace MainCharacter
+{
+    public class ClearDeathEnemies : MonoBehaviour
+    {
+        private const string MerchantEnemyTag = "MerchantEnemy";
+        private const string PeasantEnemyTag = "PeasantEnemy";
+        private const string PriestEnemyTag = "PriestEnemy";
+        private const string SoldierEnemyTag = "SoldierEnemy";
+        private const string ThiefEnemyTag = "ThiefEnemy";
+        
+        public static void Clear()
+        {
+            var gameObjects = FindObjectsOfType<GameObject>();
+            foreach (var x in gameObjects)
+            {
+                if ((x.CompareTag(MerchantEnemyTag) && x.GetComponent<Others.Merchant.AttackHandler>() is null)
+                    || (x.CompareTag(PeasantEnemyTag) && x.GetComponent<Others.Peasant.AttackHandler>() is null)
+                    || (x.CompareTag(PriestEnemyTag) && x.GetComponent<Others.Priest.AttackHandler>() is null)
+                    || (x.CompareTag(SoldierEnemyTag) && x.GetComponent<Others.Soldier.AttackHandler>() is null)
+                    || (x.CompareTag(ThiefEnemyTag) && x.GetComponent<Others.Thief.AttackHandler>() is null))
+                {
+                    x.SetActive(false);
+                }
+            }
+        }
+    }
+}
