3828d1c54b9e22ddbde45a1086b6e2f0692c98e6
I hate iu <tranhung10122003@gmail.com>
2024-10-19 22:49:06 +0700

    feat: add tech bouncing + transition gate

---
+++ Assets/Scripts/GameObjects/Texture/BouncingTexture/BouncingTexture.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Texture.BouncingTexture
+{
+    public class BouncingTexture : MonoBehaviour
+    {
+        // Force applied when the player touches the bouncing object
+        public float bounceForce = 10f;
+
+        // Detect when the player collides with the bouncing object
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            // Check if the object that collided is tagged as "Player"
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                // Get the Rigidbody2D of the player
+                Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+                // Apply an upward force to make the player bounce
+                if (playerRb != null)
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, bounceForce);
+                }
+            }
+        }
+    }
+}
