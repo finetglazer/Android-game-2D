c83999170264aa166e33a8c46342181e72a2ad05
OLogN <vinhhiepbn2003.work@gmail.com>
2024-10-21 15:18:40 +0700

    feat: Add Fire and Water obstacles, add scripts enemies running away from fire

--- Assets/Scripts/OtherCharacters/Priest/AttackHandler.cs
+++ Assets/Scripts/OtherCharacters/Priest/AttackHandler.cs
@@ -7,20 +7,24 @@ namespace OtherCharacters.Priest
         public float damageDealt = 1;
         public float distanceDetectingPlayer = 1;
         public float attackCoolDownTime = 1;                // Must > 1 second
-        public Transform lightningOccurPoint;
+        public float increaseWalkSpeedWhenChasingBy = 0.2f;
+        public float distanceTriggeringAttack = 1f;
         public GameObject cautionZone;
         public GameObject lightning;
         public GameObject lightningExplosion;
         private static readonly int Attack = Animator.StringToHash("attack");
         private static readonly int Idle = Animator.StringToHash("idle");
+        private static readonly int Walk = Animator.StringToHash("walk");
         private const string PlayerMask = "Player";
         private const string PlayerTag = "Player";
+        private Vector3 _lightningOccurPoint;
         private GameObject _player;
         private Animator _playerAnimator;
         private BoxCollider2D _characterBoxCollider;
         private Animator _characterAnimator;
         private Movement _characterMovement;
         private float _attackCoolDownTime;
+        private float _firstWalkSpeed;
         private bool _isPlayerDead;
         
         private void Start()
@@ -31,6 +35,7 @@ namespace OtherCharacters.Priest
             _characterAnimator = GetComponent<Animator>();
             _characterBoxCollider = GetComponent<BoxCollider2D>();
             _characterMovement = GetComponent<Movement>();
+            _firstWalkSpeed = GetComponent<Movement>().walkSpeed;
             _isPlayerDead = false;
         }
 
@@ -49,6 +54,10 @@ namespace OtherCharacters.Priest
             _attackCoolDownTime += Time.deltaTime;
                 
             if (!PlayerDetectedOnLeft() && !PlayerDetectedOnRight()) return;
+            
+            ChasePlayer();
+            
+            if (!PlayerIsInDamageDealtDistance()) return;
 
             if (_playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("die"))
             {
@@ -63,17 +72,7 @@ namespace OtherCharacters.Priest
             _attackCoolDownTime = 0;
             if (_isPlayerDead) return;
             
-            if (PlayerDetectedOnLeft())
-            {
-                _characterMovement.TurnLeft();
-            }
-            else if (PlayerDetectedOnRight())
-            {
-                _characterMovement.TurnRight();
-            }
-            else return;
-            
-            lightningOccurPoint.position = _player.transform.position;
+            _lightningOccurPoint = _player.transform.position;
             _characterAnimator.SetTrigger(Attack);      // All events triggered in Attack animation
         }
 
@@ -88,24 +87,55 @@ namespace OtherCharacters.Priest
             var raycastHit = Physics2D.BoxCast(_characterBoxCollider.bounds.center, _characterBoxCollider.bounds.size, 0f, Vector2.right, distanceDetectingPlayer, LayerMask.GetMask(PlayerMask));
             return raycastHit.collider is not null;
         }
+        
+        private bool PlayerIsInDamageDealtDistance()
+        {
+            var raycastHitLeft = Physics2D.BoxCast(_characterBoxCollider.bounds.center, _characterBoxCollider.bounds.size, 0f, Vector2.left, distanceTriggeringAttack, LayerMask.GetMask(PlayerMask));
+            var raycastHitRight = Physics2D.BoxCast(_characterBoxCollider.bounds.center, _characterBoxCollider.bounds.size, 0f, Vector2.right, distanceTriggeringAttack, LayerMask.GetMask(PlayerMask));
+            return raycastHitLeft.collider is not null || raycastHitRight.collider is not null;            
+        }
+        
+        private void ChasePlayer()
+        {
+            if (_player is null || PlayerIsInDamageDealtDistance())
+            {
+                return;
+            }
+            
+            if (PlayerDetectedOnLeft()) _characterMovement.TurnLeft();
+            else _characterMovement.TurnRight();
+
+            if (_characterMovement.IsWallOnLeft() && PlayerDetectedOnLeft()) return;
+            if (_characterMovement.IsWallOnRight() && PlayerDetectedOnRight()) return;
+            
+            _characterAnimator.SetTrigger(Walk);
+
+            var newWalkSpeed = _firstWalkSpeed * (1 + increaseWalkSpeedWhenChasingBy);
+            GetComponent<Movement>().walkSpeed = newWalkSpeed;
+            transform.position += Mathf.Approximately(Mathf.Sign(transform.localScale.x), 1) switch
+            {
+                true => Vector3.left * (newWalkSpeed * Time.deltaTime),
+                _ => Vector3.right * (newWalkSpeed * Time.deltaTime)
+            };
+        }
 
         private void ActivateCautionZone()
         {
             cautionZone.SetActive(true);
-            cautionZone.transform.position = new Vector3(lightningOccurPoint.position.x, cautionZone.transform.position.y, cautionZone.transform.position.z);
+            cautionZone.transform.position = new Vector3(_lightningOccurPoint.x, cautionZone.transform.position.y, cautionZone.transform.position.z);
         }
 
         private void DeActivateCautionZone()
         {
             cautionZone.SetActive(false);
             lightning.SetActive(true);
-            lightning.transform.position = new Vector3(lightningOccurPoint.position.x, lightning.transform.position.y, lightning.transform.position.z);
+            lightning.transform.position = new Vector3(_lightningOccurPoint.x, lightning.transform.position.y, lightning.transform.position.z);
         }
 
         private void ActivateLightningExplosion()
         {
             lightningExplosion.SetActive(true);
-            lightningExplosion.transform.position = new Vector3(lightningOccurPoint.position.x, lightningExplosion.transform.position.y, lightningExplosion.transform.position.z);
+            lightningExplosion.transform.position = new Vector3(_lightningOccurPoint.x, lightningExplosion.transform.position.y, lightningExplosion.transform.position.z);
         }
 
         private void DeActivateLightningExplosion()
