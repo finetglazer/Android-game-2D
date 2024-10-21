b849a956547053540a3312689052a361011c1c79
OLogN <vinhhiepbn2003.work@gmail.com>
2024-10-21 15:19:25 +0700

    feat: additional push of previous commit

---
+++ Assets/Scripts/GameObjects/Water/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Water
+{
+    public class Movement : MonoBehaviour
+    {
+        public float currentSpeed = 1f;
+        public float speedIncrement = 1f;
+        private const string SpeedAcceleratorName = "Accelerator";
+
+        private void Update()
+        {
+            transform.Translate(new Vector2(0, currentSpeed));
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.name != SpeedAcceleratorName) return;
+            currentSpeed += speedIncrement;
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjects/Water/WaveAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Water
+{
+    public class WaveAnimation : MonoBehaviour
+    {
+        private static readonly int Wave = Animator.StringToHash("wave");
+        public float delay = 1f;
+        private Animator _animator;
+        private float _clock;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _clock += Time.deltaTime;
+            if (_clock <= delay) return;
+            _animator.SetTrigger(Wave);
+        }
+    }
+}
