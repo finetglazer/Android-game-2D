387a285ef4d8b6048cd70578ca7f91038c63cd24
OLogN <vinhhiepbn2003.work@gmail.com>
2024-11-09 23:38:52 +0700

    feat: Complete New game, Game continue, Ranking APIs

---
+++ Assets/Scripts/ServerInteraction/Responses/PlayerRankingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerInteraction.Responses
+{
+    public class PlayerRankingResponse
+    {
+        public string[] usernameList;
+        public float[] finalFinishTimeList;
+        public int[] totalDeathCountList;
+        public float[] finalPointsList;
+    }
+}
