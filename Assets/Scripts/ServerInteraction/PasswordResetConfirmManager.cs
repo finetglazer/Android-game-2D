3e198c04e2f04d7b1651e6c5691ae7d2c1bfa247
I hate iu <tranhung10122003@gmail.com>
2024-11-10 16:56:25 +0700

    feat: add all functions

---
+++ Assets/Scripts/ServerInteraction/PasswordResetConfirmManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ServerInteraction.Payload;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Purchasing.MiniJSON;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ServerInteraction
+{
+    public class PasswordResetConfirmManager : MonoBehaviour
+    {
+        public TMP_InputField resetToken;
+        public TMP_InputField newPassword;
+        public TMP_InputField confirmPassword;
+        public Button submitButton;
+        public Button goToSignInButton;
+        public TMP_Text feedbackText;
+        
+        private void Start()
+        {
+            goToSignInButton.onClick.AddListener(OnGoToSignInButtonClicked);
+            submitButton.onClick.AddListener(OnSubmitButtonClicked);
+        }
+        
+        private void OnGoToSignInButtonClicked()
+        {
+            new WaitForSeconds(2f);
+            SceneManager.LoadScene("SignInScene");
+        }
+
+        private void OnSubmitButtonClicked()
+        {
+            string resetToken = this.resetToken.text;
+            string newPassword = this.newPassword.text;
+            string confirmPassword = this.confirmPassword.text;
+
+            if (!newPassword.Equals(confirmPassword))
+            {
+                feedbackText.text = "Password not match!";
+                feedbackText.color = Color.red;
+                return;
+            }
+
+            StartCoroutine(PasswordResetConfirm(resetToken, newPassword, confirmPassword));
+
+        }
+
+        IEnumerator PasswordResetConfirm(string resetToken, string newPassword, string confirmNewPassword)
+        {
+            // check the reset token
+            if (string.IsNullOrEmpty(resetToken))
+            {
+                feedbackText.text = "Invalid reset token";
+                yield break;
+            }
+            // create a request
+            string url = "http://localhost:8080/api/auth/password-reset";
+            UnityWebRequest webRequest = new UnityWebRequest(url, "POST");
+            // custom the request
+            // body
+            PasswordResetRequest passwordResetRequest = new PasswordResetRequest
+            {
+                resetToken = resetToken,
+                newPassword = newPassword,
+                confirmNewPassword = confirmNewPassword
+            };
+            // change to json string
+            string body = JsonUtility.ToJson(passwordResetRequest);
+            byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(body);
+
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            //header
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            yield return webRequest.SendWebRequest();
+            //handle the message
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                feedbackText.text = webRequest.downloadHandler.text;
+                feedbackText.color = Color.green;
+                StartCoroutine(DelayLoadScene());
+
+            }
+            else
+            {
+                feedbackText.text = webRequest.downloadHandler.text;
+                feedbackText.color = Color.red;
+            }
+        }
+
+        IEnumerator DelayLoadScene()
+        {
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene("Scenes/SignInScene");
+        }
+    }
+}
---
+++ Assets/Scripts/ServerInteraction/Payload/PasswordResetRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace ServerInteraction.Payload
+{
+    [Serializable]
+    public class PasswordResetRequest
+    {
+        public string resetToken;
+        public string newPassword;
+        public string confirmNewPassword;
+    }
+}
