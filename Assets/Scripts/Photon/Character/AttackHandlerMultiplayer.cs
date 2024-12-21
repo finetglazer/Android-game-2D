f60409cf734224c1de640cd080f881768304546a
OLogN <vinhhiepbn2003.work@gmail.com>
2024-12-21 18:58:42 +0700

    fix: Configure Scripts

--- Assets/Scripts/Photon/Character/AttackHandlerMultiplayer.cs
+++ Assets/Scripts/Photon/Character/AttackHandlerMultiplayer.cs
@@ -15,15 +15,16 @@ namespace Photon.Character
             _playerBoxCollider = GetComponent<BoxCollider2D>();
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (!photonView.IsMine) return; // Only the local player can initiate attacks
 
-            if (collision.CompareTag("Player"))
+            if (!photonView.IsMine) return; // Only the local player can initiate attacks
+            if (collision.gameObject.CompareTag("Player"))
             {
-                PhotonView enemyPhotonView = collision.GetComponent<PhotonView>();
+                PhotonView enemyPhotonView = collision.gameObject.GetComponentInParent<PhotonView>();
                 if (enemyPhotonView != null && !enemyPhotonView.IsMine)
                 {
+                    Debug.LogWarning("Attacking player: " + collision.gameObject.name);
                     // Send RPC to the enemy's owner to apply damage
                     enemyPhotonView.RPC("ApplyDamage", enemyPhotonView.Owner, damageDealt);
                 }
